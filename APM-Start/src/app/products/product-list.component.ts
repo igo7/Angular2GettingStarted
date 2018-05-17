8f227a972ef6c033dd9a4e18f5127525c67bc3e8
igo7 <mesoeuphoria@gmail.com>
2018-05-16 19:42:24 -0700

    Cant pass 14 due to Permission being denied.

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html',
+    templateUrl:'./product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string {
@@ -22,49 +24,32 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        },
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
+        constructor(private _productService: ProductService) {
 
-    onRatingClicked(message: string): void {
-        this.pageTitle = 'Product List: ' + message;
-    }
-    
-    performFilter(filterBy: string): IProduct[] {
-        filterBy = filterBy.toLocaleLowerCase();
-        return this.products.filter((product: IProduct) =>
-              product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-    }
+        }
 
-    toggleImage():void{
-        this.showImage = !this.showImage;
-    }
+        onRatingClicked(message: string): void {
+            this.pageTitle = 'Product List: ' + message;
+        }
+        performFilter(filterBy: string): IProduct[] {
+            filterBy = filterBy.toLocaleLowerCase();
+            return this.products.filter((product: IProduct) =>
+                  product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+        }
 
-    ngOnInit(): void {
-        console.log('In OnInit');
-    }
-}
+        toggleImage():void{
+            this.showImage = !this.showImage;
+        }
+
+        ngOnInit(): void {
+            this._productService.getProducts()
+                .subscribe(products => {
+                    this.products = products;
+                    this.filteredProducts = this.products;
+                },       
+                     error => this.errorMessage = <any>error);
+            }
+
+        }
